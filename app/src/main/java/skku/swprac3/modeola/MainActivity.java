720054b5dd54f0970a8f5ba98626010cf1a06aa6
NineA <yuneg11@icloud.com>
2018-05-16 18:58:58 +0900

    Add files to VCS

--- app/src/main/java/skku/swprac3/modeola/MainActivity.java
+++ app/src/main/java/skku/swprac3/modeola/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity
         serviceStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent serviceStart = new Intent(getApplicationContext(), ArtikNofiticationService.class);
+                Intent serviceStart = new Intent(getApplicationContext(), ArtikNotificationService.class);
                 serviceStart.putExtra("AccessToken", ".................");
                 startService(serviceStart);
             }
